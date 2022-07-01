1983a7d7bc3ad5a3173ee8a60797cbc12b0615a4
Dust621 <1627193837@qq.com>
2022-07-02 00:05:25 +0800

    ⭐安装MUI

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,7 +3,8 @@ import React from "react";
 
 class Home extends React.Component {
   render() {
-    return <Button variant="outlined">你好，世界</Button>;
+    const subject = "ReallyFun";
+    return <Button variant="outlined">你好，{subject}! </Button>;
   }
 }
 
